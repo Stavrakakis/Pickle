e8593c28226cf1573e224da5c76dcd6459a696e2
Nico Stavrakakis <nico.stavrakakis@aberdeen-asset.com>
2015-11-05 08:34:28 +0000

    add test project and initial passing test

---
+++ Pickle.Web.Tests/Providers/TokenUsernameProviderTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Pickle.Web.Tests.Providers
+{
+    public class TokenUsernameProviderTests
+    {
+        [Fact]
+        public void GetsTheUsernameFromTheClaimsWhenAuthenticated()
+        {
+            Assert.True(true);
+        }
+    }
+}
